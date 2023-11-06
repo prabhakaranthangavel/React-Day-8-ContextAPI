54f692ebb63bd8e601f4576885bcba992fe29fea
prabhakaranthangavel <karangeetha9600@gmail.com>
2023-11-06 15:48:26 +0530

    Content Added & Updated

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { createContext, useState } from 'react';
+import Profile from './components/Profile';
+
+// Create a context object using the createContext function from React
+const ProfileContext = createContext();
+
+
+// Define a state variable called profileName using the useState hook and set its initial alue to an empty string.
+function App() {
+    const [profileName, setProfileName] = useState('');
+  return (
+      <div>
+          {/* Wrap the Profile component with the context provider component */}
+          {/* passing the profileName state and its corresponding setter function as the value prop.*/}
+          <ProfileContext.Provider value={{ profileName, setProfileName }}>
+              <Profile />
+          </ProfileContext.Provider>
+    </div>
+  )
+}
+
+export {App as default, ProfileContext};
